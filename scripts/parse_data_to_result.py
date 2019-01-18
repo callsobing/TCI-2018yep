d9e9c6c95f7cd6f03d317b82c26744a0cf60d77b
callsobing <callsobing@gmail.com>
2019-01-18 19:46:55 +0800

    讀三個檔案  產生四種資料

--- scripts/parse_data_to_result.py
+++ scripts/parse_data_to_result.py
@@ -1,10 +1,9 @@
-import os
 import xlrd
 from opencc import OpenCC
 import re
 
-
 customer_info = {}
+#讀入試題統計
 wb = xlrd.open_workbook("../data/20181227考试测试_云课堂(考试结果)_Min.Yue_20190107161730 (1).xlsx", on_demand=True)
 
 sheet = wb.sheet_by_name("正考结果")
@@ -18,6 +17,36 @@ for i in range(1, nrows):
     employee_score = float(row_data[5])
     employee_score_dict[employee_name] = employee_score
 
+#讀入考試結果
+wb = xlrd.open_workbook("../data/20181227考试测试_云课堂(考试结果)_Min.Yue_20190107161730 (1).xlsx", on_demand=True)
+
+sheet = wb.sheet_by_name("正考结果")
+customer_service_pair = {}
+nrows = sheet.nrows
+employee_score_dict = {}
+
+for i in range(1, nrows):
+    row_data = sheet.row_values(i)
+    employee_name = row_data[1]
+    employee_score = float(row_data[5])
+    employee_score_dict[employee_name] = employee_score
+
+#讀入分組名單
+wb = xlrd.open_workbook("../data/20181227考试测试_云课堂(考试结果)_Min.Yue_20190107161730 (1).xlsx", on_demand=True)
+
+sheet = wb.sheet_by_name("正考结果")
+customer_service_pair = {}
+nrows = sheet.nrows
+employee_score_dict = {}
+
+for i in range(1, nrows):
+    row_data = sheet.row_values(i)
+    employee_name = row_data[1]
+    employee_score = float(row_data[5])
+    employee_score_dict[employee_name] = employee_score
+
+
+
 output_fh = open("../data/employee_ranking.txt", "w", encoding="UTF-8")
 employee_score_dict_sorted = sorted(employee_score_dict.items(), key=lambda kv: kv[1], reverse=True)
 for kv_pair in employee_score_dict_sorted:
@@ -25,9 +54,10 @@ for kv_pair in employee_score_dict_sorted:
     match = regex.search(kv_pair[0])
     cc = OpenCC('t2s')
     try:
-        output_fh.write("%s\t%s\n" % (cc.convert(match[1]).encode("utf-8"), kv_pair[1]))
+        output_fh.write("%s\t%s\n" % (cc.convert(match[1]), kv_pair[1]))
         print("%s\t%s" % (cc.convert(match[1]), kv_pair[1]))
     except:
-        output_fh.write("%s\t%s\n" % (cc.convert(kv_pair[0]).encode("utf-8"), kv_pair[1]))
+        output_fh.write("%s\t%s\n" % (cc.convert(kv_pair[0]), kv_pair[1]))
 
 output_fh.close()
+
