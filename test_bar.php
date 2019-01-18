c70c9cb71b322e0f93c0d7c9ad01b3e4dde7e700
callsobing <callsobing@gmail.com>
2019-01-18 14:44:23 +0800

    抓取frame大小自動scale要畫多滿

--- test_bar.php
+++ test_bar.php
@@ -91,7 +91,7 @@ while (!feof($file)) {
                 .duration(3000)
                 .attr({
                     'x':function(d){
-                        return d.w;
+                        return (((screenWidth - 80)/<?php echo($rankings[$employee_key[0]]); ?>) * d.w );
                     }
                 })
                 .tween('number',function(d){
