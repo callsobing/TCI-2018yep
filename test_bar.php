0d2af4aebb56e3855a40b913e59708b2eac196b2
callsobing <callsobing@gmail.com>
2019-01-21 19:05:07 +0800

    update aspect ratio

--- test_bar.php
+++ test_bar.php
@@ -3,7 +3,7 @@
 <html lang="zh-Hans">
 
 <?php
-$employee_score_file = "data/top10_employee.txt";
+$employee_score_file = "data/output_top10_employee.txt";
 $file = fopen($employee_score_file, "r");
 $employee_key = [];
 $rankings =array();
