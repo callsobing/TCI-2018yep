6b1f7643b5deb4e9b2d38cd5b9a21ea1a13bda45
callsobing <callsobing@gmail.com>
2019-01-22 12:31:56 +0800

    update

--- bar_employee.php
+++ bar_employee.php
@@ -98,8 +98,8 @@ while (!feof($file)) {
                 .duration(5000)
                 .attr({
                     'x':function(d){
-                        if((((screenWidth)/<?php echo($max_score); ?>) * d.w ) - 150 > 0) {
-                            return (((screenWidth) /<?php echo($max_score); ?>) * d.w) - 150;
+                        if((((screenWidth)/<?php echo($max_score); ?>) * d.w ) - 100 > 0) {
+                            return (((screenWidth) /<?php echo($max_score); ?>) * d.w) - 100;
                         }
                         return 0;
                     }
