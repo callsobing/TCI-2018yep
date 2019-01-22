6932a3831bc399df85316124c9113b54053441e8
callsobing <callsobing@gmail.com>
2019-01-22 12:26:41 +0800

    update

--- bar_employee.php
+++ bar_employee.php
@@ -74,7 +74,7 @@ while (!feof($file)) {
                 .duration(5000)
                 .attr({
                     'width':function(d){
-                        return (((screenWidth - 150)/<?php echo($max_score); ?>) * d.w );
+                        return (((screenWidth)/<?php echo($max_score); ?>) * d.w );
                     }
                 });
 
@@ -98,7 +98,10 @@ while (!feof($file)) {
                 .duration(5000)
                 .attr({
                     'x':function(d){
-                        return (((screenWidth - 150)/<?php echo($max_score); ?>) * d.w );
+                        if((((screenWidth)/<?php echo($max_score); ?>) * d.w ) - 150 > 0) {
+                            return (((screenWidth) /<?php echo($max_score); ?>) * d.w) - 150;
+                        }
+                        return 0;
                     }
                 })
                 .tween('number',function(d){
