bbbc729cd290b7137078c62f064cd1edeeca363a
callsobing <callsobing@gmail.com>
2019-01-22 20:34:12 +0800

    更新顯示方式

--- donut6.php
+++ donut6.php
@@ -6,8 +6,8 @@ $rankings =array();
 $avg_score = 0;
 $contents = fgets($file);
 $items = preg_split('/\t/', $contents);
-$display_string = $items[0] . ":" . mb_substr( $items[1],0,10,"utf-8") . "...";
-$avg_score = floatval($items[2]);
+$display_string = mb_substr( $items[0],0,20,"utf-8") . "...";
+$avg_score = floatval($items[1]);
 
 ?>
 
