5083c5122c34e2fe2c5119228eac061ea5ae6292
callsobing <callsobing@gmail.com>
2019-01-24 11:22:59 +0800

    update aspect ratio

--- bar_employee.php
+++ bar_employee.php
@@ -27,7 +27,8 @@ while (!feof($file)) {
     <title></title>
     <style>
         text {
-            font-size: 30px
+            font-size: 30px;
+            font-weight: bolder;
         }
     </style>
     <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
