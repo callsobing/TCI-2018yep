d5d386628e0af7cf112457b6e630341e7fe1700d
callsobing@gmail.com <lemontree>
2019-01-25 15:04:54 +0800

    加入加分環節

--- scripts/parse_data_to_result.py
+++ scripts/parse_data_to_result.py
@@ -160,7 +160,7 @@ group_score["E1-張雅婷"] += 1
 group_score["C3-鄧蓓"] += 2
 group_score["F6-汪興一"] += 3
 group_score["D3-王金良"] += 1
-group_score["C6-劉珊珊"] += 10
+group_score["C6-劉珊珊"] += 2
 sorted_by_value = sorted(group_score.items(), key=lambda kv: kv[1], reverse=True)
 
 count = 0
